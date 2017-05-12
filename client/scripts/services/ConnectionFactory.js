34470ff1de41b27185e7f63c8fcd331c405dd62e
NathPaiva <hello@nathpaiva.com.br>
2017-05-12 08:59:03 -0300

    block close connection

--- client/scripts/services/ConnectionFactory.js
+++ client/scripts/services/ConnectionFactory.js
@@ -1,41 +1,62 @@
-var stores = ['negociacoes'];
-var version = 4;
-var dbName = 'aluraframe';
+var ConnectionFactory = (function() {
+  const stores = ['negociacoes'],
+    version = 4,
+    dbName = 'aluraframe';
 
-class ConnectionFactory {
-  constructor() {
-    throw new Error('Não pode criar uma instancia de ConnectionFactory');
-  }
-  static getConnection() {
-    return new Promise((resolve, reject) => {
-      let openRequest = window.indexedDB.open('aluraframe', 4);
+  var connection = null,
+    close = null;
 
-      openRequest.onupgradeneeded = e => {
-        ConnectionFactory._createConnection(e.target.result);
-      };
+  return class ConnectionFactory {
+    constructor() {
+      throw new Error('Não pode criar uma instancia de ConnectionFactory');
+    }
+    static getConnection() {
+      return new Promise((resolve, reject) => {
+        let openRequest = window.indexedDB.open('aluraframe', 4);
 
-      openRequest.onsuccess = e => {
-        console.log('e.target.result', e.target.result);
-        resolve(e.target.result);
-      };
+        openRequest.onupgradeneeded = e => {
+          ConnectionFactory._createConnection(e.target.result);
+        };
 
-      openRequest.onerror = e => {
-        console.error(e.target.error);
-        reject(e.target.error.name);
-      };
-    });
-  }
+        openRequest.onsuccess = e => {
+          if (!connection) {
+            connection = e.target.result;
+            close = connection.close.bind(connection);
+            // Ou não faz o bind e chama no close
+            // Reflect.apply(close, connection, []);
+            connection.close = function() {
+              throw new Error('Não pode fechar a connection');
+            }
 
-  static _createConnection(connection) {
+          }
+          resolve(connection);
+        };
 
-    stores.forEach(store => {
-      if (connection.objectStoreNames.contains(store)) {
-        connection.deleteObjectStore(store);
-      }
-
-      connection.createObjectStore(store, {
-        autoIncrement: true
+        openRequest.onerror = e => {
+          console.error(e.target.error);
+          reject(e.target.error.name);
+        };
       });
-    });
+    }
+
+    static _createConnection(connection) {
+
+      stores.forEach(store => {
+        if (connection.objectStoreNames.contains(store)) {
+          connection.deleteObjectStore(store);
+        }
+
+        connection.createObjectStore(store, {
+          autoIncrement: true
+        });
+      });
+    }
+
+    static closeConnection() {
+      if (connection) {
+        close();
+        connection = null;
+      }
+    }
   }
-}
+})();
